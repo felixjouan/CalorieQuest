153390df7ff67eb53ab502ddc35a8319ea1addce
Roman <romanperie04@gmail.com>
2025-03-11 12:52:04 +0100

    commit roman 5

--- src/js/selection.js
+++ src/js/selection.js
@@ -273,14 +273,15 @@ export default class selection extends Phaser.Scene {
 
     function ramasserBurger(un_player, un_burger){ //fonction pour ramasser les burgers
       un_burger.disableBody(true,true); //enlève la texture du burger
-      speedjump = 0.5 speedjump ; //on divise par 2 la vitesse de saut
+      speedjump = 0.5*speedjump ; //on divise par 2 la vitesse de saut
       //setTimeout(speedjump=speedjump/10 , 500000) ;
       setTimeout(() => {
         speedjump = speedjump*2;
       }, 5000); // 5000 pour  secondes
       //La variable speedjump ne va être remise à sa valeur initiale qu'après un retard de 5 secondes
-      score += 50 ;
+      score -= 50 ;
     }//fin de la fonction ramasserBurger
+    this.physics.add.overlapp(player, groupe_burgers,ramasserBurger, null, this) ;
 
 
   }//fin de la fonction create
