ac96a62033b59f54d4bf1238d1319c245235b970
Roman <romanperie04@gmail.com>
2025-03-11 16:48:08 +0100

    12

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -3,8 +3,7 @@ import selection from "/src/js/selection.js";
 import niveau1 from "/src/js/scene/niveau1.js";
 import niveau2 from "/src/js/scene/niveau2.js";
 import niveau3 from "/src/js/scene/niveau3.js";
-import p_acceuil from "/src/scene/js/p_acceuil.js";
-import p_acceuil from "/src/js/scenes/p_acceuil.js";
+import p_acceuil from "/src/js/scene/p_acceuil.js";
 
 
 // configuration générale du jeu
