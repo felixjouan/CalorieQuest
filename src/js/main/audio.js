37fd4d8a20170afe3d52fddf6a8493b96db499ce
Felix <Felix>
2025-03-12 11:07:39 +0100

    sound

--- src/js/main/audio.js
+++ src/js/main/audio.js
@@ -1,20 +1,37 @@
 export default class AudioManager {
     constructor(scene) {
         this.scene = scene;
+        this.sound1 = null;
+        this.sound2 = null;
+        this.soundElevator = null;
     }
 
     preload() {
         this.scene.load.audio("musique_1", "assets/globalmusique.mp3");
+        this.scene.load.audio("sound3", "assets/sound3.mp3");
         this.scene.load.audio("ascenseur_sound", "assets/musique_ascenseur.mp3");
     }
 
     create() {
         var sound1;
         var sound_elevator;
-        sound1 = this.scene.sound.add('musique_1');
-        sound_elevator=this.scene.sound.add('ascenseur_sound');
+        var sound2;
+        sound1 = this.scene.sound.add("musique_1", { loop: true, volume: 0.5 });
+        sound2 = this.scene.sound.add("sound3", { loop: true, volume: 0.5 });
+        sound_elevator=this.scene.sound.add("ascenseur_sound", { loop: false, volume: 0.5 });
         sound_elevator.play();
+        this.sound1.on("complete", () => {  //sound2 se lance quand sound1 s arrete 
+            this.playMusic2();
+        });
+ 
+    }
 
-        
+    stopElevatorAndPlayMusic1() {
+        if (this.soundElevator && this.soundElevator.isPlaying) {
+            this.soundElevator.stop(); // Arrête la musique d'ascenseur
+        }
+        if (this.sound1 && !this.sound1.isPlaying) {
+            this.sound1.play(); // Démarre la musique de fond
+        }
     }
 }
